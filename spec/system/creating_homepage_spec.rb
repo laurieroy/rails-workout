e7189bced1b1991e3888d93bd053ab366833e17d
lauretteroy <laurieroy.dev@gmail.com>
2021-11-22 21:56:07 -0600

    Create homepage spec

---
+++ spec/system/creating_homepage_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+RSpec.describe "Creating Homepage" do
+	scenario do
+		visit "/"
+
+		expect(page).to have_link("Athlete's Den")
+		expect(page).to have_link("Home")
+		expect(page).to have_content("Workout Lounge")
+		expect(page).to have_content("Show off your workout")
+
+
+	end
+end
