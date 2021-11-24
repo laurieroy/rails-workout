c84583bc0b260a04fdbac6bdda7be50398bfa2aa
lauretteroy <laurieroy.dev@gmail.com>
2021-11-23 22:15:57 -0600

    Refactor tests

--- spec/system/users/signout_spec.rb
+++ spec/system/users/signout_spec.rb
@@ -1,11 +1,14 @@
 require "rails_helper"
 
-RSpec.describe "User registration form" do
+RSpec.describe "User sign out" do
 	scenario "with valid credentials" do
+			@user = create(:user)
+			login_as(@user)
+
 			visit root_path
-			# sleep(5)
 
 			click_link "Sign out"
+
 			expect(page).to have_content("Signed out successfully.")
 			expect(page).to	have_current_path(root_path)
 	end
