825248d6076993bfca2c217965c1f187edb23745
lauretteroy <laurieroy.dev@gmail.com>
2021-11-30 15:28:17 -0600

    Write editing exercise spec

--- spec/factories/exercises.rb
+++ spec/factories/exercises.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :exercise do
     duration_in_min { 1 }
-    workout { "MyText" }
+    workout { "MyWorkout" }
     workout_date { "2021-11-24" }
-    user { nil }
+    user
   end
 end
---
+++ spec/system/exercises/editing_exercise_spec.rb
@@ -0,0 +1,30 @@
+require "rails_helper"
+
+RSpec.describe "Editing exercise" do
+	before do
+		@edited_user = create :user
+		@exercise_to_edit = create :exercise
+		# @exercise_to_edit =  create :exercise(user: @edited_user)
+		@exercise_to_edit.user_id = @edited_user.id
+		login_as(@edited_user)
+	end
+
+	scenario "succeeds with valid inputs" do
+		byebug
+		visit "/"
+
+		click_link "My Workouts"
+
+		path = "/users/#{@edited_user.id}/exercises/#{@xercise_to_edit.user_id}/edit"
+		link = "a[href=\'#{path}\']"
+		click_on link
+		
+
+		fill_in "Duration",	with: 45
+		click_button "Update Exercise"
+
+		expect(page).to have_text("Exercise has not been updated")
+		expect(page).to have_content(45)
+		expect(page).to_not have_content(1)
+	end
+end
