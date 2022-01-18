1016128a137aa6e1d7bd056ff604ad13023f044e
lauretteroy <laurieroy.dev@gmail.com>
2022-01-18 11:30:46 -0800

    Fix editing exercise

--- spec/system/exercises/editing_exercise_spec.rb
+++ spec/system/exercises/editing_exercise_spec.rb
@@ -2,28 +2,35 @@ require "rails_helper"
 
 RSpec.describe "Editing exercise" do
 	before do
-		@edited_user = create :user
-		@exercise_to_edit = create :exercise
-		# @exercise_to_edit =  create :exercise(user: @edited_user)
+		# @edited_user = create :user
+		# @exercise_to_edit = create!(:exercise)
+		# # @exercise_to_edit =  create :exercise(user: @edited_user)
+		# @exercise_to_edit.user_id = @edited_user.id
+		# login_as(@edited_user)
+		@edited_user = user_with_exercises
+		@exercise_to_edit =  @edited_user.exercises.first
 		@exercise_to_edit.user_id = @edited_user.id
+		@exercise_to_edit.workout = "exercise_to_edit"
+		@exercise_to_edit.save
+
 		login_as(@edited_user)
 	end
 
 	scenario "succeeds with valid inputs" do
 		visit "/"
-
+	
 		click_link "My Workouts"
 
+		# visit edit_user_exercise_path(@exercise_to_edit)
 		path = "/users/#{@edited_user.id}/exercises/#{@exercise_to_edit.id}/edit"
 		link = "a[href=\'#{path}\']"
-		sleep(5)
-		click_on "Edit"
-		
+		find(link, match: :first).click()
+
 		fill_in "Duration",	with: 45
 		click_button "Update Exercise"
 
-		expect(page).to have_text("Exercise has not been updated")
+		expect(page).to have_text("Exercise has been updated")
 		expect(page).to have_content(45)
-		expect(page).to_not have_content(1)
+		expect(page).to_not have_content("(min): 1")
 	end
 end
