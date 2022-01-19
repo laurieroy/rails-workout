d3568dd2dab865d0ab38effacb646fd10986b973
lauretteroy <laurieroy.dev@gmail.com>
2022-01-18 22:02:11 -0800

    Add full_name test to user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "returns the full_name for an user" do
+    user = build :user, first_name: "Jane", last_name: "Doe"
+
+    expect(user.full_name).to eq("Jane Doe")
+  end
 end
