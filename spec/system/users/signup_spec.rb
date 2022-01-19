89b777258301c5564782354c864fd4071b4fb5e0
lauretteroy <laurieroy.dev@gmail.com>
2022-01-18 21:49:27 -0800

    Require first, last names for user

--- spec/system/users/signup_spec.rb
+++ spec/system/users/signup_spec.rb
@@ -24,6 +24,27 @@ RSpec.describe "User registration", js:true do
 			expect(page).to have_content("You have signed up successfully")
 			# expect(page).to have_content("Welcome, #{@user.email}!")
 			expect(page).to have_content("Signed in as #{@user.email}")
+	end
 
+	scenario "with valid credentials" do
+		visit root_path
+
+		click_link "Sign up"
+		expect(page).to	have_current_path(new_user_registration_path, wait: 3)
+
+		@user = build(:user)
+
+		within("#new_user") do
+			fill_in :user_first_name, with: ""
+			fill_in :user_last_name,	with: ""
+			fill_in :user_email, with: @user.email
+			fill_in :user_password, with: @user.password
+			fill_in :user_password_confirmation, with: @user.password
+
+			click_button "Sign up"
+		end
+
+		expect(page).to have_content("First name can't be blank")
+		expect(page).to have_content("Last name can't be blank")
 	end
 end
