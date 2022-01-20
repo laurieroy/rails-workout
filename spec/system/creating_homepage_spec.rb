cf267ca07dca29a9d5242d3e16bd043a87a588dd
lauretteroy <laurieroy.dev@gmail.com>
2022-01-19 23:06:00 -0800

    Remove test for Home link removed from navbar

--- spec/system/creating_homepage_spec.rb
+++ spec/system/creating_homepage_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe "Creating Homepage" do
 		visit "/"
 
 		expect(page).to have_link("Athlete's Den")
-		expect(page).to have_link("Home")
+		# expect(page).to have_link("Home")
 		expect(page).to have_content("Workout Lounge")
 		expect(page).to have_content("Show off your workout")
 
