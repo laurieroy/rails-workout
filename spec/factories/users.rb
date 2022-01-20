c56287dc1f031d83a2049e85f1a992bd6903ca11
lauretteroy <laurieroy.dev@gmail.com>
2022-01-19 16:58:16 -0800

    Add traits to user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,10 +5,21 @@ FactoryBot.define do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
   end
+
 end
 
 def user_with_exercises(exercises_count: 5)
   FactoryBot.create(:user) do |user|
     FactoryBot.create_list(:exercise, exercises_count, user: user)
   end
+
+  # trait :invalid_first_name_user do
+  #   first_name {""}
+  # end
+
+  # trait :invalid_last_name_user do
+  #   last_name {""}
+  # end
+  # factory :invalid_user, traits: [:invalid_last_name_user]
+
 end
