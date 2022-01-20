ff5cc1b245f3ee061fd59e29f4f89c5455b118a3
lauretteroy <laurieroy.dev@gmail.com>
2022-01-19 18:23:55 -0800

    Add searching name on homepage test

---
+++ spec/features/dashboards/searching_for_users_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+RSpec.describe "Searching for user" do
+	before do
+		@u1 = create(:user, last_name: "Doe")
+		@u2 = create(:user, last_name: "Doe")
+	end
+
+	scenario "with name of existing user returns all matches" do
+		visit root_path
+		# byebug
+		fill_in "search_name",	with: "Doe" 
+		click_button "Search"
+
+		expect(page).to have_content(@u1.full_name) 
+		expect(page).to have_content(@u2.full_name) 
+		expect(current_path).to eq("/dashboards/search") 
+	end
+end
