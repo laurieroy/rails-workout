1ac9ca172b4d3f3af2e676b616080a8d9cb7c826
lauretteroy <laurieroy.dev@gmail.com>
2022-01-20 19:28:01 -0800

    Add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+3.times do |i|
+	User.create(
+		email: "user-#{i+1}@example.com", 
+		first_name: "user-#{i+1}",
+		last_name: "User",
+		password: "password", 
+		password_confirmation: "password"
+	)
+end
+
+User.all.each do |u|
+	3.times do |i|
+		u.exercises.create(duration_in_min: i+1, workout: "workout#{i+1}", workout_date: Date.yesterday  )
+	end
+end
+
+User.last.exercises.destroy
