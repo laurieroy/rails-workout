fa82373737117a419b275f440960ef5eb97a3de7
lauretteroy <laurieroy.dev@gmail.com>
2022-01-20 18:31:07 -0800

    Add showing_friend_workout_spec

---
+++ spec/features/friendships/showing_friend_workout_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+RSpec.describe "Listing exercise" do
+	before do
+		@u1 = create(:user)
+		@u2 = user_with_exercises
+pp@u1
+pp@u2
+		login_as(@u1)
+
+		@following = Friendship.create(user: @u1, friend: @u2)
+	end
+
+	scenario "shows friend's workouts for the past 7 days" do
+		visit "/"
+
+		click_link "My Workouts"
+		click_link @u2.full_name
+
+		expect(page).to have_text("#{@u2.full_name}'s Exercises")
+		expect(page).to have_content(@u2.workout.first)
+	end
+end
